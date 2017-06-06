ff4ef190e3e56e71847766179d73b2b68f347f3f
Pat Sissons <patricksissons@gmail.com>
2017-06-06 09:31:42 -0600

    moving monad quote to a new slide

--- presentation/01-splash.jsx
+++ presentation/01-splash.jsx
@@ -12,18 +12,5 @@ export default (
     <Tags.Text textColor='tertiary'>
       <Tags.Code>RxJS</Tags.Code> and <Tags.Code>React</Tags.Code>
     </Tags.Text>
-    <Tags.Appear>
-      <Tags.BlockQuote>
-        <Tags.Quote textSize='2rem'>
-          All told, a monad in <Tags.Code>X</Tags.Code> is just a monoid in the category of endofunctors of <Tags.Code>X</Tags.Code>, with product <Tags.Code>×</Tags.Code> replaced by composition of endofunctors and unit set by the identity endofunctor.
-        </Tags.Quote>
-        <Tags.Cite textAlign='right'>Saunders Mac Lane</Tags.Cite>
-        <Tags.Text textAlign='right' textSize='2rem'>
-          <Tags.Link textColor='quartenary' href='http://books.google.com/books?id=MXboNPdTv7QC&pg=PA138&lpg=PA138&dq=%22monoid+in+the+category+of+endofunctors%22+mac+lane&source=bl&ots=feQWTkH2Uw&sig=tv-1JwaMOygKGmFE2vM2FhJVS9o&hl=en&ei=5iWsTJCkBIPSsAPQwJ36Aw&sa=X&oi=book_result&ct=result&resnum=1&ved=0CBIQ6AEwAA#v=onepage&q&f=false'>
-            Categories for the Working Mathematician
-          </Tags.Link>
-        </Tags.Text>
-      </Tags.BlockQuote>
-    </Tags.Appear>
   </Tags.Slide>
 );
